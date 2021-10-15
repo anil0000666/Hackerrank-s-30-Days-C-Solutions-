1e202f61e3f51b32a2eb6d45fe64e0bbb06da8c0
Tohidujjaman Hoque <tohi7479@gmail.com>
2021-10-15 23:21:46 +0530

    Adding solution of day 1 in c

---
+++ Day 1/solution.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+
+int main() {
+    char input_string[105]; 
+    gets(input_string);
+    printf("Hello, World.\n");
+    puts(input_string);
+    return 0;
+}
